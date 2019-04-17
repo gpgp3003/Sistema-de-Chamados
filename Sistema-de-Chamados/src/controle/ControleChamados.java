f2ea50182a82d0b9d3ae053f78d9b592a5e7eeea
Gian Piero <gianpgazzillo99@gmail.com>
2019-04-16 21:38:39 -0300

    Fix ortografia outprints

--- Sistema-de-Chamados/src/controle/ControleChamados.java
+++ Sistema-de-Chamados/src/controle/ControleChamados.java
@@ -140,30 +140,30 @@ public class ControleChamados {
             			break;
             			
             		case 4 :
-            			prioridade = "Crítica";
+            			prioridade = "Cratica";
             			break;
             	}
                 relatorio += "\n" + "--------" + "\nData de abertura do chamado: " +
                     	ref.getData() + 
-                    	"\nHorário de abertura do chamado: " + ref.getHora() + 
-                    	"\nTítulo do chamado: " + ref.getTitulo() + 
-                    	"\nCódigo do chamado: " + ref.getCodigo() +
-                    	"\nDescrição do chamado: " + ref.getDescricao() + 
+                    	"\nHorario de abertura do chamado: " + ref.getHora() + 
+                    	"\nTatulo do chamado: " + ref.getTitulo() + 
+                    	"\nCadigo do chamado: " + ref.getCodigo() +
+                    	"\nDescriaao do chamado: " + ref.getDescricao() + 
                     	"\nPrioridade do chamado: " + prioridade + 
                     	"\nStatus do chamado: " + ref.getStatus() + 
                     	"\nTipo de problema do chamado: " + ref.getTipoProblema() + 
-                    	"\nTécnico responsável pelo chamado: " + ref.getTecnico() + 
+                    	"\nTacnico responsavel pelo chamado: " + ref.getTecnico() + 
                     	"\nCliente requisitor do chamado: " + ref.getCliente() + "\n";
                 
                 for(RegistroChamado rc : registros){
                 	if(rc.getChamado().equals(ref)){
-                		relatorio += "\nRelatório de registros de acompanhamento:" + 
+                		relatorio += "\nRelatario de registros de acompanhamento:" + 
                 	"\nData: " + rc.getData() + 
                 	"\nHora: " + rc.getHora() + 
                 	"\nAssunto: " + rc.getAssunto() +
-                	"\nTécnico responsável: " + rc.getTecnico().getNome() +
+                	"\nTacnico responsavel: " + rc.getTecnico().getNome() +
                 	"\nCausa do problema: " + ref.getCausaProblema() + 
-                	"\nSolução do problema: " + ref.getSolucaoProblema();
+                	"\nSoluaao do problema: " + ref.getSolucaoProblema();
                 	}
                 }
             }
@@ -190,7 +190,7 @@ public class ControleChamados {
 
         String detalhes = "\n" + "--------" + "\n" + "Data de abertura do chamado: "
                 + c.getData() + "\n" + "Hororio de abertura do chamado: " + c.getHora() + "\n"
-                + "Titulo do chamado: " + c.getTitulo() + "\n" + "Descriï¿½ï¿½o do chamado: " + c.getDescricao() + "\n"
+                + "Titulo do chamado: " + c.getTitulo() + "\n" + "Descriaao do chamado: " + c.getDescricao() + "\n"
                 + "Prioridade do chamado" + c.getPrioridade() + "\n" + "Status do chamado: " + c.getStatus() + "\n"
                 + "Tipo de problema do chamado: " + c.getTipoProblema() + "\n" + "Tecnico responsovel pelo chamado: "
                 + c.getTecnico() + "\n" + "Cliente requisitor do chamado: " + c.getCliente() + "\n";
